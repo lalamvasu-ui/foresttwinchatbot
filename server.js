b6cb1e7cf33ed133ac4697f3dc533915ed860972
lalamvasu-ui <lalam.vasu@fusionpact.com>
2026-05-05 17:03:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const knowledge = require("./knowledge");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type"],
+  credentials: false
+}));
 app.use(express.json());
 app.use(express.static("public"));
 
